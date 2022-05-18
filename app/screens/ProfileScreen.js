86905fcdc0963b8d7c1bb4428420e845c95b3f50
Lizbeth García <66501578+Lizg19@users.noreply.github.com>
2022-05-18 16:02:13 -0500

    Vista Concerte , Perfil

--- app/screens/ProfileScreen.js
+++ app/screens/ProfileScreen.js
@@ -4,6 +4,7 @@ import { Avatar } from 'react-native-elements'
 import { useTheme, Text } from 'react-native-paper'
 import * as styles from '../../assets/styles/appStyles'
 import Feather from 'react-native-vector-icons/Feather'
+import FontAwesome from 'react-native-vector-icons/FontAwesome'
 
 export const ProfileScreen = () => {
   const paperTheme = useTheme();
@@ -33,7 +34,84 @@ export const ProfileScreen = () => {
         style={[styles.commons.text_footer, {
           marginTop: 35,
           color: paperTheme.dark == true ? 'white' : '#05375a'
-        }]}>Password</Text>
+        }]}>Nombre</Text>
+
+      <View style={styles.commons.action}>
+        <Feather
+          name="lock"
+          color={paperTheme.dark == true ? styles.colors.cultured : "#05375a"}
+          size={20} />
+        <TextInput
+          placeholder='Your Password'
+          placeholderTextColor={paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray}
+          style={[styles.commons.textInput, { color: paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray }]}
+          autoCapitalize="none"
+
+        />
+        {/* <TouchableOpacity onPress={() => { }}>
+          <Feather
+            name={data.secureTextEntry ? "eye-off" : "eye"}
+            color="grey"
+            size={20}
+          />
+        </TouchableOpacity> */}
+      </View>
+      <Text
+        style={[styles.commons.text_footer, {
+          marginTop: 35,
+          color: paperTheme.dark == true ? 'white' : '#05375a'
+        }]}>Apellido</Text>
+
+      <View style={styles.commons.action}>
+        <Feather
+          name="lock"
+          color={paperTheme.dark == true ? styles.colors.cultured : "#05375a"}
+          size={20} />
+        <TextInput
+          placeholder='Your Password'
+          placeholderTextColor={paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray}
+          style={[styles.commons.textInput, { color: paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray }]}
+          autoCapitalize="none"
+
+        />
+        {/* <TouchableOpacity onPress={() => { }}>
+          <Feather
+            name={data.secureTextEntry ? "eye-off" : "eye"}
+            color="grey"
+            size={20}
+          />
+        </TouchableOpacity> */}
+      </View>
+      <Text style={[styles.commons.text_footer, {
+        marginTop: 35,
+        color: paperTheme.dark == true ? 'white' : '#05375a'
+      }]}>Email</Text>
+      <View style={styles.commons.action}>
+        <FontAwesome
+          name="user-o"
+          color={paperTheme.dark == true ? styles.colors.cultured : "#05375a"}
+          size={20} />
+        <TextInput
+          placeholder='Your Password'
+          placeholderTextColor={paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray}
+          style={[styles.commons.textInput, { color: paperTheme.dark == true ? styles.colors.cultured : styles.colors.lightGray }]}
+          autoCapitalize="none"
+
+        />
+        {data.check_textInputChange ?
+          <Animatable.View animation="bounceIn">
+            <Feather
+              name="check-circle"
+              color="green"
+              size={20}
+            />
+          </Animatable.View> : <></>}
+      </View>
+      <Text
+        style={[styles.commons.text_footer, {
+          marginTop: 35,
+          color: paperTheme.dark == true ? 'white' : '#05375a'
+        }]}>Nombre</Text>
 
       <View style={styles.commons.action}>
         <Feather
