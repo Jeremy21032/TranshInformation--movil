549811d729bb8ec95e47e1f16a8948bb64bbaf06
Jeremy210321 <jeremy.leon@epn.edu.ec>
2022-06-26 22:43:26 -0500

    Versión lista para apk

--- app/screens/SignInScreen.js
+++ app/screens/SignInScreen.js
@@ -103,13 +103,9 @@ export const SignInScreen = ({ navigation }) => {
         const user = userCredential.user;
         console.log(user);
         console.log("Sign in!");
-        let userData = await getPersonalRol(user.email);
-        let verify = await getPersonalInfomation();
-        var userFinal = await Object.assign({}, userData, verify);
-
-        loginAction({ type: "sign-in", data: userFinal });
-        setIsLoading(false);
-        setModalVisibleError(false);
+        // let userData = await getPersonalRol(user.email);
+        // let verify = await getPersonalInfomation();
+        // var userFinal = await Object.assign({}, userData, verify);
       })
       .catch((error) => {
         setIsLoading(false);
